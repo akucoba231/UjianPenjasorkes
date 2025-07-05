6dbfa2e8835156e24e55ab5460f66615e8d32e6e
akucoba231 <aku.coba231@gmail.com>
2025-07-05 13:09:01 +0700

    Delete klikkanan.js

--- klikkanan.js
+++
@@ -1,7 +0,0 @@
-// Opsi 1: Mencegah klik kanan di seluruh halaman (pada elemen <body>)
-
-document.body.addEventListener('contextmenu', function(e) {
-    e.preventDefault(); // Mencegah munculnya menu konteks
-    // Opsional: Anda bisa menambahkan alert atau log ke konsol
-    // alert('Klik kanan dinonaktifkan di halaman ini!');
-});
